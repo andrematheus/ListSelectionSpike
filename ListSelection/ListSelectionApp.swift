63a2f75059baf213690c53c71a23d49116e2cb8c
André Roque Matheus <amatheus@ligpo.dev>
2020-11-12 20:00:23 -0300

    Initial Commit

---
+++ ListSelection/ListSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListSelectionApp.swift
+//  ListSelection
+//
+//  Created by André Roque Matheus on 12/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ListSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
